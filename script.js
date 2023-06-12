cd60dc2f92036dffacfb08cd1933e8c636f19850
ZoeyYanling <122327135+ZoeyYanling@users.noreply.github.com>
2023-06-12 23:46:56 +0800

    Update script.js
    Part5 Add a listener Event

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+//Part5 Add a listener for click events on submit button to save data
+var submitBtn = $('.submitBtn');
+var name = $('#name').text();
+var nameInput = $('#nameInput').val();
+var email = $('#email').text();
+var emailInput = $('#emailInput').val();
+var interest = $('#interest').text();
+var interestInput = $('#interestInput').val();
+
+submitBtn.on('click',function(){   
+   
+    localStorage.setItem(name,JSON.stringify(nameInput) );
+    localStorage.setItem(email,JSON.stringify(emailInput));
+    localStorage.setItem(interest,JSON.stringify(interestInput));
+});
